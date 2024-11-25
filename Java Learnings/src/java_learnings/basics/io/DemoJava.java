495e96b319cec6fb6aa8cca18d7f7de6379004ef
ankush-dot <ankushpawar229@gmail.com>
2024-11-26 03:12:58 +0530

    Commit-2

--- Java Learnings/src/java_learnings/basics/io/DemoJava.java
+++ Java Learnings/src/java_learnings/basics/io/DemoJava.java
@@ -8,6 +8,9 @@ public class DemoJava {
 	public static void main(String[] args) {
 		
 		ArrayList<Integer> al = new ArrayList<Integer>(List.of(10,30,49,48));
+		for(int i: al) {
+			System.out.println(i);
+		}
 		
 	}
 

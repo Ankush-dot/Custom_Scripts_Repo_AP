f96632df5f8cada5d507d132a0687dc91138826a
Ankush Pawar <ankushpawar229@gmail.com>
2024-12-09 15:17:59 +0530

    Adding Spring Project Commit-1

---
+++ spring-scripts/src/main/java/com/cutomescripts/ap/spring_scripts/SpringScriptsApplication.java
@@ -0,0 +1,13 @@
+package com.cutomescripts.ap.spring_scripts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScriptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScriptsApplication.class, args);
+	}
+
+}
---
+++ spring-scripts/src/test/java/com/cutomescripts/ap/spring_scripts/SpringScriptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutomescripts.ap.spring_scripts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScriptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
